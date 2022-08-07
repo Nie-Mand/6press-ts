d8f5e2d70d321a4c1c5c0b0b0b370f2ac227d0ca
Nie-Mand <chill.out.x19@gmail.com>
2022-08-07 16:48:27 +0100

    fixed an error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import 'dotenv/config'
 import run from './main'
 
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        id: string
+        role: string
+      }
+    }
+  }
+}
+
 run()
